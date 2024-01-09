ed95f2613b2ca4deccc15c9bf21cea74d44bcdb9
LauraDemarco17 <demarcolaura2003@gmail.com>
2024-01-09 17:25:34 +0000

    Added model files and data in db

---
+++ app/Models/Car.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    use HasFactory;
+
+    //enable mass assignment
+    protected $fillable = ['model', 'year', 'salesperson_email'];
+
+}
---
+++ app/Models/Manufacturer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Manufacturer extends Model
+{
+    use HasFactory;
+
+    //enable mass assignment
+    protected $fillable = ['name', 'address', 'phone'];
+
+    public function cars(){
+        //establish one to many relationship with the model contact
+        return this->hasMany(Car::class); //Manufacturer model has many cars
+    }
+
+
+}
