4a1ab1f5bfd0dfa624394434a74a253c322e56c4
LauraDemarco17 <demarcolaura2003@gmail.com>
2024-01-10 07:58:39 +0000

    Fixed filter function

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,4 @@
-document.getElementById('filter_manufacturer_id').addEventListener('change',
-function(){
-    //generate a query string
-    //http://sss-lab:800/cars?manufacturer_id=2
-    let manufacturerId = this.value || this.options[this.selectedIndex].value;
-    window.location.href = window.location.hred.split("?")[0] + '?' + manufacturerId
-    //we split to eliminate this scenario of multiple concatenations
-    //
-})
+document.getElementById('filter_manufacturer_id').addEventListener('change', function () {
+    let manufacturerId = this.value || this.options[this.selectedIndex].value
+    window.location.href = window.location.href.split('?')[0] + '?manufacturer_id=' + manufacturerId
+  })
