daa0045b24d8673192a4b0ecd06301d6f8efbc4b
LauraDemarco17 <demarcolaura2003@gmail.com>
2024-01-10 06:58:46 +0000

    Implemented partial view for filtering

---
+++ resources/views/cars/_filter.blade.php
@@ -0,0 +1,19 @@
+<div class="row">
+                  <div class="col-md-6"></div>
+                  <div class="col-md-6">
+                    <div class="row">
+                      <div class="col">
+                        <div class="input-group mb-3">
+                          <select id="filter_manufacturer_id" name="manufacturer_id" class="custom-select">            
+                           <!-- <option value ="" selected>All Companies</option> -->
+                          @foreach($manufacturers as $id => $name)
+                                <!--<option value="{{$id}}" selected>{{$name}}</option> -->
+                                <option {{$id == request('manufacturer_id') ? 'selected' : ''}} value={{ $id }}>{{ $name }}</option>
+                            @endforeach
+                          </select>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
